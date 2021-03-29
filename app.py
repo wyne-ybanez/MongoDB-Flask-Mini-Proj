6c8c3e32326a80163bf8a322669843c618303704
wyne-ybanez <wyne_01@icloud.com>
2021-03-29 21:02:54 +0000

    Added tasks template and connecting URI to mongoDB, Bug found, not connecting properly

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import os
-from flask import Flask
-
-# We need this so that it can import for Heroku
-if os.path.exists('env.py'):
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-@app.route('/')
-def hello():
-    return 'Hello World ... again!'
+mongo = PyMongo(app)
 
 
-# Get info from env.py
-if __name__ == '__main__':
-    app.run(host = os.environ.get('IP'),
-            port = int(os.environ.get('PORT')),
-            debug = True) # Make sure to change this to false during project deployment
+@app.route("/")
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
